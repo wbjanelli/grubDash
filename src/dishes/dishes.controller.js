fd5b5b44732b968d91a4ceb7661cf0ae000f9fc5
William Janelli <bradub@Williams-MBP.lan>
2023-11-28 09:34:43 -0800

    dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,17 +1,80 @@
 const path = require("path");
-
-// Use the existing dishes data.
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary.
 const nextId = require("../utils/nextId");
 
-// This function lists all existing dish data.
 function list(req, res, next) {
   res.send({ data: dishes });
 }
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-module.exports = {
-  list
+const dishExists = (req, res, next) => {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((d) => d.id === dishId);
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    next();
+  } else {
+    next({ status: 404, message: `No matching id is found for '${dishId}'` });
+  }
+};
+
+function read(req, res, next) {
+  const foundDish = res.locals.dish;
+  res.send({ data: foundDish });
 }
+
+const validatesPrice = (req, res, next) => {
+  const input = req.body.data.price;
+  if (!input || input <= 0 || typeof input !== "number") {
+    next({ status: 400, message: "Dish must have a price that is an integer greater than 0" });
+  } else {
+    next();
+  }
+};
+
+const validateFor = (property) => {
+  return function (req, res, next) {
+    const input = req.body.data[property];
+    if (!input) {
+      next({ status: 400, message: `Dish must include a ${property}` });
+    } else {
+      next();
+    }
+  };
+};
+
+function create(req, res, next) {
+  const { data: { name, description, price, image_url } } = req.body;
+  let newDish = { id: nextId(), name, description, price, image_url };
+  dishes.push(newDish);
+  res.status(201).send({ data: newDish });
+}
+
+const validatesCorrectIdToUpdate = (req, res, next) => {
+  const { dishId: routeId } = req.params;
+  const dishId = req.body.data.id;
+  if (dishId && routeId !== dishId) {
+    next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${dishId}, Route: ${routeId}`,
+    });
+  } else {
+    next();
+  }
+};
+
+function update(req, res, next) {
+  const foundDish = res.locals.dish;
+  const { data: { name, description, price, image_url } } = req.body;
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+  res.send({ data: foundDish });
+}
+
+module.exports = {
+  list,
+  read: [dishExists, read],
+  create: [validatesPrice, validateFor("name"), validateFor("description"), validateFor("image_url"), create],
+  update: [dishExists, validatesCorrectIdToUpdate, validatesPrice, validateFor("name"), validateFor("description"), validateFor("image_url"), update],
+};
