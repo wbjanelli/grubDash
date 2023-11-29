170d855276484a3caf18823fbd4c0e1e70b99f5e
William Janelli <bradub@Williams-MacBook-Pro.local>
2023-11-29 14:39:21 -0800

    orders and comments

--- src/dishes/dishes.router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("./dishes.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
-
-router
-  .route("/")
-  .get(controller.list)
-module.exports = router;
